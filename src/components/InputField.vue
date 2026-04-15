008de0cad16d3e1d8d941f87ecad509012ab28b2
Gabriel Nhaia <gabriel.nhaia@gmail.com>
2026-04-15 20:49:14 -0300

    feat: componente InputField reutilizável

---
+++ src/components/InputField.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-input
+    v-bind="$attrs"
+    :model-value="modelValue"
+    :label="label"
+    :type="type"
+    :rules="rules"
+    outlined
+    dense
+    lazy-rules
+    @update:model-value="$emit('update:modelValue', $event)"
+  >
+    <template v-if="icon" #prepend>
+      <q-icon :name="icon" />
+    </template>
+    <slot />
+  </q-input>
+</template>
+
+<script setup>
+defineOptions({ inheritAttrs: false })
+
+defineProps({
+  modelValue: { type: [String, null], default: null },
+  label: { type: String, required: true },
+  type: { type: String, default: 'text' },
+  icon: { type: String, default: undefined },
+  rules: { type: Array, default: () => [] },
+})
+
+defineEmits(['update:modelValue'])
+</script>
